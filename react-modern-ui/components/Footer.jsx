6dd4a5a080c1809182b1ec68c7e748e79d67a5a4
Ojochogwu Dickson <07afrik@gmail.com>
2023-06-13 13:43:42 -0400

    fix-dir

--- react-modern-ui/components/Footer.jsx
+++ react-modern-ui/components/Footer.jsx
@@ -29,7 +29,7 @@ const Footer = () => (
             className="w-[24px] h-[24px] object-contain"
           />
           <span className="font-normal text-[16px] text-white">
-            Enter Metaverse
+            Enter Past
           </span>
         </button>
       </div>
@@ -38,9 +38,9 @@ const Footer = () => (
         <div className="mb-[50px] h-[2px] bg-white opacity-10" />
 
         <div className="flex items-center justify-between flex-wrap gap-4">
-          <h4 className="font-extrabold text-[24px] text-white">METAVERUS</h4>
+          <h4 className="font-extrabold text-[24px] text-white">TTX</h4>
           <p className="font-normal text-[14px] text-white opacity-50">
-            Copyright © 2021 - 2022 Metaversus. All rights reserved.
+            Copyright © 2021 - 2023 TT XGROUP. All rights reserved.
           </p>
 
           <div className="flex gap-4">
