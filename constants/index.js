c047f4df3e7109a8c13604476863b5235a07a021
Ojochogwu Dickson <07afrik@gmail.com>
2023-06-13 16:58:03 -0400

    track-lost-files

---
+++ constants/index.js
@@ -0,0 +1,153 @@
+export const exploreWorlds = [
+    {
+      id: "world-1",
+      imgUrl: "/planet-01.png",
+      title: "2070",
+    },
+    {
+      id: "world-2",
+      imgUrl: "/planet-02.png",
+      title: "The NEW WORLD",
+    },
+    {
+      id: "world-3",
+      imgUrl: "/planet-03.png",
+      title: "The Metaverse",
+    },
+    {
+      id: "world-4",
+      imgUrl: "/planet-04.png",
+      title: "Paradise Of Dinosaurs",
+    },
+    {
+      id: "world-5",
+      imgUrl: "/planet-05.png",
+      title: "Ambriant Mystery",
+    },
+  ];
+  
+  export const startingFeatures = [
+    "Find a world that suits you and you want to enter",
+    "Enter the world by reading basmalah to be safe",
+    "No need to beat around the bush, just stay on the gas and have fun",
+  ];
+  
+  export const newFeatures = [
+    {
+      imgUrl: "/vrpano.svg",
+      title: "A new world",
+      subtitle:
+        "we have the latest update with new world for you to try never mind",
+    },
+    {
+      imgUrl: "/headset.svg",
+      title: "More realistic",
+      subtitle:
+        "In the latest update, your eyes are narrow, making the world more realistic than ever",
+    },
+  ];
+  
+  export const insights = [
+    {
+      imgUrl: "/planet-06.png",
+      title: "The launch of the Metaverse makes Elon musk ketar-ketir",
+      subtitle:
+        "Magna etiam tempor orci eu lobortis elementum nibh tellus molestie. Diam maecenas sed enim ut sem viverra alique.",
+    },
+    {
+      imgUrl: "/planet-07.png",
+      title: "7 tips to easily master the madness of the Metaverse",
+      subtitle:
+        "Vitae congue eu consequat ac felis donec. Et magnis dis parturient montes nascetur ridiculus mus. Convallis tellus id interdum",
+    },
+    {
+      imgUrl: "/planet-08.png",
+      title: "With one platform you can explore the whole world virtually",
+      subtitle:
+        "Quam quisque id diam vel quam elementum. Viverra nam libero justo laoreet sit amet cursus sit. Mauris in aliquam sem",
+    },
+  ];
+  
+  export const socials = [
+    {
+      name: "twitter",
+      url: "/twitter.svg",
+    },
+    {
+      name: "linkedin",
+      url: "/linkedin.svg",
+    },
+    {
+      name: "instagram",
+      url: "/instagram.svg",
+    },
+    {
+      name: "facebook",
+      url: "/facebook.svg",
+    },
+  ];
+  export const slideImages = [
+    {
+      name: "Component A",
+      images: [
+        {
+          src: "/travel-03.webp",
+          alt: "Component A Image 4",
+        },
+        {
+          src: "/v1-01.webp",
+          alt: "Component A Image 1",
+        },
+        {
+          src: "/v1-02.webp",
+          alt: "Component A Image 2",
+        },
+        {
+          src: "/v1-03.webp",
+          alt: "Component A Image 3",
+        },
+      ],
+    },
+    {
+      name: "Component B",
+      images: [
+        {
+          src: "/travel-04.webp",
+          alt: "Component A Image 4",
+        },
+        {
+          src: "/v2-01.webp",
+          alt: "Component A Image 1",
+        },
+        {
+          src: "/v2-02.webp",
+          alt: "Component A Image 2",
+        },
+        {
+          src: "/v2-03.webp",
+          alt: "Component A Image 3",
+        },
+      ],
+    },
+    {
+      name: "Component C",
+      images: [
+        {
+          src: "/travel-06.webp",
+          alt: "Component A Image 4",
+        },
+        {
+          src: "/v3-01.webp",
+          alt: "Component A Image 1",
+        },
+        {
+          src: "/v3-02.webp",
+          alt: "Component A Image 2",
+        },
+        {
+          src: "/v3-03.webp",
+          alt: "Component A Image 3",
+        },
+      ],
+    },
+  ];
